3583c77b0ac10a12604ce28923c6ef55243cb54b
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-09-05 19:17:06 +0200

    Ejemplo de un getAll

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Prueba;
 
 class HomeController extends Controller
 {
-    public function __invoke(){
-        return 'desde el controlador';
+    public function __invoke()
+    {
+        $all = Prueba::get();
+        return $all;
     }
 }
